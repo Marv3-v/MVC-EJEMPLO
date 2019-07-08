2bd434287da281da347179db08c88c3f1c1bea0f
Marv <marvin4alarcon3@gmail.com>
2019-07-08 15:33:14 -0600

    commit inicial;

---
+++ controllers/main.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Main extends Controller {
+    function __construct() {
+        parent::__construct();
+        $this->view->render('main/index');
+        
+        echo "<p>Nuevo controlador Main</p>";
+    }
+
+    function saludar() {
+            echo "<p>HOLLAA</p>";
+    }
+}
+
+?>
